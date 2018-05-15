df4c8328d953da2741ae47658fc6af282c6912d0
Jacob Goodall <jgoodall@opp.local>
2018-05-15 16:48:17 -0500

    nav dropdown

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,26 +9,30 @@ class Header extends Component {
     render(){
         return (
             <div>
-            <div className="top-bar-wrapper">
-                <header className="container" >
-                    <nav className="row">
-                        <div className="col-md-5">
-                            <a href="/"><img src={logo} /></a>
-                        </div>
-                        <div className="col-md-7">
-                        <ul className="nav-right">
-                            <li className="nav-right-links"><Link to='/work'>Work</Link></li>
-                            <li className="nav-right-links"><Link to='/capabilites'>Capabilites</Link></li>
-                            <li className="nav-right-links"><Link to='/services'>Services</Link></li>
-                            <li className="nav-right-links"><Link to='/about'>About Us</Link></li>
-                            <li className="nav-right-links"><Link to='/get'>Get in Touch</Link></li>
-                            <li className="nav-right-links"><Link to='/notes'>Key Notes</Link></li>
-                        </ul>
-                        </div>
-                    </nav>
-                </header>
-            </div>
-            <div style={{height: '100px'}}></div>
+                <div className="top-bar-wrapper">
+                    <header className="container" >
+                        <nav className="row">
+                            <div className="col-md-5">
+                                <a href="/"><img src={logo} alt="logo"/></a>
+                            </div>
+                            <div className="col-md-7">
+                            <ul className="nav-right">
+                                <li id='nav-work' className="nav-right-links"><Link to='/work'>Work</Link>
+                                <div className="work-dropdown">
+                                    <h4>hi!</h4>
+                                </div>
+                                </li>
+                                <li className="nav-right-links"><Link to='/capabilites'>Capabilites</Link></li>
+                                <li className="nav-right-links"><Link to='/services'>Services</Link></li>
+                                <li className="nav-right-links"><Link to='/about'>About Us</Link></li>
+                                <li className="nav-right-links"><Link to='/get'>Get in Touch</Link></li>
+                                <li className="nav-right-links"><Link to='/notes'>Key Notes</Link></li>
+                            </ul>
+                            </div>
+                        </nav>
+                    </header>
+                </div>
+                <div style={{height: '100px'}}></div>
             </div>  
         )
     }
